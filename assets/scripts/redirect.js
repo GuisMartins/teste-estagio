e336134a4d5ab67edfa4f83ccaa608a8f7ddffc8
Guilherme Martins <guis.1177@gmail.com>
2022-05-12 18:16:59 -0300

    Criação da pasta assets, arquivo index e início da marcações

---
+++ assets/scripts/redirect.js
@@ -0,0 +1,13 @@
+setTimeout(function () {
+  /************ Redirecionamento condicional **********/
+  /*
+        if (confirm('Deseja ser redirecionado para vollpilates.com.br?')) {
+          window.location.href = 'https://www.vollpilates.com.br';
+        }
+      }, 15000);
+
+      /************ Redirecionamento automático **********/
+
+  alert('Você esta sendo redirecionado para vollpilates.com.br');
+  window.location.href = 'https://www.vollpilates.com.br';
+}, 1500);
